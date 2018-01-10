b54b1bf942cfc0b96c20b83fe8d068f8d3bf5c57
Miguel Vieira <jmvieira@gmail.com>
2018-01-10 11:28:23 +0000

    Added another option to the form to specify the resources to count. The resource class field has been converted to a query field.

--- src/Site/BlockLayout/ResourceStats.php
+++ src/Site/BlockLayout/ResourceStats.php
@@ -21,25 +21,34 @@ class ResourceStats extends AbstractBlockLayout
         SitePageRepresentation $page = null,
         SitePageBlockRepresentation $block = null
     ) {
-        $text = new Text("o:block[__blockIndex__][o:data][resource-class]");
+        $text = new Text("o:block[__blockIndex__][o:data][query]");
+        $resource = new Text("o:block[__blockIndex__][o:data][resource]");
         $label = new Text("o:block[__blockIndex__][o:data][label]");
 
         if ($block) {
-            $text->setAttribute('value', $block->dataValue('resource-class'));
+            $text->setAttribute('value', $block->dataValue('query'));
+            $resource->setAttribute('value', $block->dataValue('resource'));
             $label->setAttribute('value', $block->dataValue('label'));
         }
 
         $html = '<div class="field"><div class="field-meta">';
-        $html .= '<label>' . $view->translate('Resource Class Id') . '</label>';
+        $html .= '<label>' . $view->translate('Query') . '</label>';
         $html .= '<a href="#" class="expand"></a>';
-        $html .= '<div class="collapsible"><div class="field-description">' . $view->translate('Resource classe (id) to generate counts for.') . '</div></div>';
+        $html .= '<div class="collapsible"><div class="field-description">' . $view->translate('Display resource counts for this query, for example to get a count of all the persons the query should be `resource_class_id=1304`.') . '</div></div>';
         $html .= '</div>';
         $html .= '<div class="inputs">' . $view->formRow($text) . '</div>';
         $html .= '</div>';
 
         $html .= '<div class="field"><div class="field-meta">';
+        $html .= '<label>' . $view->translate('Resource') . '</label>';
+        $html .= '<a href="#" class="expand"></a><div class="collapsible"><div class="field-description">' . $view->translate('Resources to search on: items, media, etc.') . '</div></div>';
+        $html .= '</div>';
+        $html .= '<div class="inputs">' . $view->formRow($resource) . '</div>';
+        $html .= '</div>';
+
+        $html .= '<div class="field"><div class="field-meta">';
         $html .= '<label>' . $view->translate('Label') . '</label>';
-        $html .= '<a href="#" class="expand"></a><div class="collapsible"><div class="field-description">' . $view->translate('Resource class label') . '</div></div>';
+        $html .= '<a href="#" class="expand"></a><div class="collapsible"><div class="field-description">' . $view->translate('Label for the results') . '</div></div>';
         $html .= '</div>';
         $html .= '<div class="inputs">' . $view->formRow($label) . '</div>';
         $html .= '</div>';
@@ -51,24 +60,20 @@ class ResourceStats extends AbstractBlockLayout
     {
         $results = array();
 
-        $resourceClass = $block->dataValue('resource-class');
-        $site = $block->page()->site();
+        parse_str($block->dataValue('query'), $query);
 
         // commenting site_id for now because most of the images don't seem to
         // be associated with a site
+        // $site = $block->page()->site();
         // $query['site_id'] = $site->id();
-        $query['resource_class_id'] = $resourceClass;
 
-        $counter = 0;
+        $resource = $block->dataValue('resource');
 
-        foreach (['items', 'media'] as $resource) {
-            $response = $view->api()->search($resource, $query);
-            $content = $response->getContent();
+        $response = $view->api()->search($resource, $query);
+        $content = $response->getContent();
 
-            if ($content) {
-                $counter = $counter + count($content);
-                $results[$content[0]->resourceClass()->uri()] = $counter;
-            }
+        if ($content) {
+            $results[$content[0]->resourceClass()->uri()] = count($content);
         }
 
         return $view->partial('common/block-layout/resource-stats', [
